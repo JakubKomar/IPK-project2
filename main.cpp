009dcb005708017e046edec8743ef1d0772e9137
komca12 <57370991+komca12@users.noreply.github.com>
2021-04-08 12:57:20 +0200

    ++

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     signal(SIGINT, userExit);
     signal(SIGTERM, userExit);
     string interface="";
-    int port=-1,n=-1;
+    int port=-1,n=1;
     bool I=false,tcp=false, udp=false,arp=false,icmp=false;
 
     for(int i=1;i<argc;i++){
@@ -79,21 +79,86 @@ int main(int argc, char **argv)
 
     if(interface=="")
         displayInterfaces();
+    
+    char errbuf[PCAP_ERRBUF_SIZE];
 
+    bpf_u_int32 mask;		/* Our netmask */
+	bpf_u_int32 net;		/* Our IP */
+    if (pcap_lookupnet(interface.c_str(), &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", interface.c_str(), errbuf);
+		net = 0;
+		mask = 0;
+	}
+    debug("my interface:\n");
+    debug("ip:"+dec2Ip(net)+"\n");
+    debug("mask:"+dec2Ip(mask)+"\n");
+    debug("---------------------------------\n");
 
-    pcap_t *handle;
-    char error_buffer[PCAP_ERRBUF_SIZE];
-	handle = pcap_open_live(interface.c_str(), BUFSIZ, 1, 1000, error_buffer);
+    pcap_t *handle= pcap_open_live(interface.c_str(), BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", interface.c_str(), error_buffer);
+		fprintf(stderr, "Couldn't open device %s: %s\n", interface.c_str(), errbuf);
 		return(2);
     }
+    string filter_exp=filterGen(port,tcp,udp,arp,icmp);
+    struct bpf_program fp;	
+    if (pcap_compile(handle, &fp, filter_exp.c_str(), 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter\n");
+		return(2);
+	}
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter\n");
+        return(2);
+	}
+    struct pcap_pkthdr header;
+    for(int i=0;i<n;i++){
+        const u_char *packet = pcap_next(handle, &header);
+		printf("Jacked a packet with length of [%d]\n", header.len);
 
 
+	
+    }
+	pcap_close(handle);
     
 
-    startSnifing();
     return 0;
+}
+string filterGen(int port,bool tcp,bool udp,bool arp,bool icmp){
+    string filter_exp;
+    if(port>0&&port<65535)
+    {
+        filter_exp="port "+to_string(port)+" and ";
+    }
+    if (tcp||udp||arp||icmp)
+    {
+         filter_exp=filter_exp+"(";
+        if(tcp)
+            filter_exp=filter_exp+"tcp";
+        if(udp){
+            if(tcp)
+                filter_exp=filter_exp+" or udp";
+            else 
+                filter_exp=filter_exp+"udp";
+        }
+        if(arp){
+            if(tcp||udp)
+                filter_exp=filter_exp+" or arp";
+            else 
+                filter_exp=filter_exp+"arp";
+        }
+        if(icmp){
+            if(tcp||udp||arp)
+                filter_exp=filter_exp+" or icmp";
+            else 
+                filter_exp=filter_exp+"icmp";
+        }
+        filter_exp=filter_exp+")";
+    }
+    else
+    {
+        filter_exp=filter_exp+"(tcp or udp or arp or icmp)";
+    }
+    debug("filtering exp::\n"+filter_exp+"\n---------------------------------\n");
+    return filter_exp;
 }
 void startSnifing(){
 
@@ -134,4 +199,12 @@ void debug(string masegge)
 void userExit(int signum ){
 
     exit(-2);
+}
+string dec2Ip(int dec){
+    unsigned char bytes[4];
+    bytes[1] = dec & 0xFF;
+    bytes[2] = (dec >> 8) & 0xFF;
+    bytes[3] = (dec >> 16) & 0xFF;
+    bytes[4] = (dec >> 24) & 0xFF;   
+    return to_string(bytes[1])+"."+to_string(bytes[2])+"."+to_string(bytes[3])+"."+to_string(bytes[4]);        
 }
--- main.h
+++ main.h
@@ -14,3 +14,13 @@ void debug(string masegge);
 void displayInterfaces();
 void userExit(int signum);
 void startSnifing();
+string dec2Ip(int dec);
+string filterGen(int port,bool tcp,bool udp,bool arp,bool icmp);
+
+
+	/* Ethernet header */
+	struct sniff_ethernet {
+		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+		u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+		u_short ether_type; /* IP? ARP? RARP? etc */
+	};
